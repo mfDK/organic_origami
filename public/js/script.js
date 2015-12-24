496f4e6707821b6688f9304a6e5c30abb04301c3
Danny Kim <dkim7474@gmail.com>
2015-12-24 12:30:25 -0500

    added an image gallery to action page and hover effects to the gallery

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
 $(".fade").fadeIn(4000);
+$(".dummy").hover(function(){
+	$(this).css("opacity","1");
+	}, function(){
+		$(this).css("opacity","0.3");
+	});
 
 });
 
